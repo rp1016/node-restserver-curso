69c611d2fecb1b355f42125881207a9314157889
PARRR028 <rafael.x.parra.-nd@disney.com>
2019-04-06 23:29:01 -0500

    REST SERVER - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,16 @@
 // Puerto
 // =========================
 process.env.PORT = process.env.PORT || 3000;
+
+// =========================
+// Entorno
+// =========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================
+// Base de datos
+// =========================
+let user = 'strider';
+let password = 'mTnNGqWOvsGW6UDs';
+let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb + srv: //strider:' + password + '@cluster0-a9pk1.mongodb.net/test';
+process.env.URLDB = urlDB;
---
+++ server/routes/usuario.js
@@ -0,0 +1,160 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const app = express();
+
+const Usuario = require('../models/usuario');
+
+
+
+//mTnNGqWOvsGW6UDs
+//strider
+//mongodb+srv://strider:<password>@cluster0-a9pk1.mongodb.net/test
+// Petición de tipo GET
+app.get("/usuario", function(req, res) {
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }, 'nombre  email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    cuantos: conteo,
+                    usuarios
+                });
+            });
+
+        })
+
+});
+
+
+// Petición de tipo POST
+app.post("/usuario", function(req, res) {
+    let body = req.body;
+
+
+
+    // utilizar modelo para grabar en la base de datos
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            message: 'bad request, name is required.'
+        });
+
+    } else {
+
+        let usuario = new Usuario({
+            nombre: body.nombre,
+            email: body.email,
+            password: bcrypt.hashSync(body.password, 10),
+            role: body.role
+        });
+
+        // grabado en base de datos
+        usuario.save((err, usuarioDB) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+
+            res.status(201).json({
+                ok: true,
+                usuario: usuarioDB
+            });
+        });
+
+
+    }
+
+
+});
+
+
+// Petición de tipo PUT
+app.put("/usuario/:id", function(req, res) {
+    let id = req.params.id;
+
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+    });
+
+
+});
+
+
+
+// Petición de tipo DELETE
+app.delete("/usuario/:id", function(req, res) {
+
+    let id = req.params.id;
+
+    let cambiaEstado = {
+        estado: false
+    };
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+
+    });
+
+
+
+
+
+});
+
+//exportar la libreria
+module.exports = app;
